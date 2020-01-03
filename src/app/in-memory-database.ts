54c98e1a675662a5cdae19b25460b877c0771f37
hiquecs <hiquecs@gmail.com>
2020-01-03 00:04:13 -0300

    aprimoramentos listar e deletar

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,17 @@
+import {InMemoryDbService} from  "angular-in-memory-web-api";
+import{ Category } from "./pages/categories/shared/category.model";
+
+export class InMemoryDatabase implements InMemoryDbService {
+    createDb(){
+        const categories: Category[] = [
+
+            { id: 1, nome: 'Moradia', description: 'Pagamentos de Contas da Casa'},
+            { id: 2, nome: 'Saúde', description: 'Plano de Saúde e Remédios'},
+            { id: 3, nome: 'Lazer', description: 'Cinema, parques, praia, etc'},
+            { id: 4, nome: 'Salário', description: 'Recebimento de Salário'},
+            { id: 5, nome: 'Freelas', description: 'Trabalhos como freelancer'}
+        ];
+
+        return { categories }
+    }
+}
